c0f2c64b6c68094a082c3eb9d3774595e18e95e1
zhang chuan <zhangchuan>
2021-01-25 10:32:09 +0000

    update

--- src/config.py
+++ src/config.py
@@ -4,12 +4,12 @@ DEVICE = "cuda"
 MAX_LEN = 128
 TRAIN_BATCH_SIZE = 64
 VALID_BATCH_SIZE = 32
-EPOCH = 1
+EPOCH = 3
 WARMUP_STEPS = 100
 EVALUATION_STEPS = 500 
 BERT_PATH = "../inputs/bert-base-chinese"
-MODEL_SAVE_PATH = "../model_file/"
-TRAINING_FILE = "../data/debug_train.csv"
+MODEL_SAVE_PATH = "../outputs/"
+TRAINING_FILE = "../data/wz_train.csv"
 TOKENIZER = transformers.BertTokenizer.from_pretrained(
         BERT_PATH,
         do_lower_case = True
