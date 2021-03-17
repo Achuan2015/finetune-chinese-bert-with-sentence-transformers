99dd843c247c080cb1b600b6af49c4f8fe1dc3a8
zhangchuan <zhangchuan2017@gmail.com>
2021-03-18 00:24:02 +0800

    update

--- demo/demo_milvus.py
+++ demo/demo_milvus.py
@@ -4,7 +4,7 @@ import numpy as np
 from milvus import Milvus, IndexType, MetricType, Status
 
 # default to connenct uri address
-uri = 'tcp://150.158.219.39:29530'
+uri = 'tcp://*.*.*.*:29530'
 
 # vector dimention
 _DIM = 8
@@ -23,21 +23,21 @@ def main():
             }
     # show collections in Milvus server
     _, collections = milvus.list_collections()
-    
+
     # 创建 collection
     milvus.create_collection(param)
     # 创建 collection partion
     milvus.create_partition(collection_name, partition_tag)
-    
+
     print(f'collections in Milvus: {collections}')
     # Describe demo_collection
     _, collection = milvus.get_collection_info(collection_name)
     print(f'descript demo_collection: {collection}')
-    
+
     # build fake vectors
     vectors = [[random.random() for _ in range(_DIM)] for _ in range(10)]
     vectors1 = [[random.random() for _ in range(_DIM)] for _ in range(10)]
-    
+
     status, id = milvus.insert(
             collection_name=collection_name,
             records=vectors,
@@ -46,7 +46,7 @@ def main():
     print(f'status: {status} | id: {id}')
     if not status.OK():
         print(f"insert failded: {status}")
-    
+
     status1, id1 = milvus.insert(
             collection_name=collection_name,
             records=vectors1,
@@ -55,7 +55,7 @@ def main():
     print(f'status1: {status1} | id1: {id1}')
 
     ids_deleted = list(range(10))
-    
+
     status_delete = milvus.delete_entity_by_id(collection_name=collection_name,
             id_array=ids_deleted)
     if status_delete.OK():
@@ -69,7 +69,7 @@ def main():
     # Get demo_collection row count
     status, result = milvus.count_entities(collection_name)
     print(f"demo_collection row count: {result}")
-    
+
     # Obtain raw vectors by providing vector ids
     status, result_vectors = milvus.get_entity_by_id(collection_name, list(range(10, 20)))
 
@@ -77,7 +77,7 @@ def main():
     index_param = {
             'nlist': 2
             }
-    
+
     # create ivflat index in demo_collection
     status = milvus.create_index(
             collection_name,
@@ -88,19 +88,19 @@ def main():
 
     # use the top 10 vectors for similarity search
     query_vectors = vectors1[0:2]
-    
+
     # execute vector similariy search
     search_param = {
             "nprobe": 16
         }
-    
+
     param = {
             'collection_name': collection_name,
             'query_records': query_vectors,
             'top_k': 3,
             'params': search_param
     }
-    
+
     status, results = milvus.search(**param)
     if status.OK():
         if results[0][0].distance == 0.0:
